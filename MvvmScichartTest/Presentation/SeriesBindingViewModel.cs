e5adf5594cafe2536058089dbcd84eeb949ce6c1
fabricekfr <fabricekfr@mail.ru>
2016-12-09 14:20:05 -0500

    FastImpulseRenderableSeries
    FastImpulseRenderableSeries mais ca va etre reverte

--- MvvmScichartTest/Presentation/SeriesBindingViewModel.cs
+++ MvvmScichartTest/Presentation/SeriesBindingViewModel.cs
@@ -11,20 +11,26 @@ namespace Presentation
     public class SeriesBindingViewModel : ViewModelBase
     {
         public ObservableCollection<IRenderableSeriesViewModel> RenderableSeriesViewModels { get; set; }
+        public IDataSeries ChartData { get; set; }
 
         public SeriesBindingViewModel()
         {
             var dataService = new DesignDataService();
             var data = dataService.GetData().ToList();
 
-            var lineDataSeries = new XyDataSeries<DateTime, double>();
-            lineDataSeries.Append(data.Select(d => d.Date), data.Select(d => d.Weight));
+            var xyDataSeries = new XyDataSeries<DateTime, double>();
+            xyDataSeries.Append(data.Select(d => d.Date), data.Select(d => d.Weight));
 
             RenderableSeriesViewModels = new ObservableCollection<IRenderableSeriesViewModel>()
             {
-                new ColumnRenderableSeriesViewModel {DataSeries = lineDataSeries},
-                new LineRenderableSeriesViewModel {DataSeries = lineDataSeries}
+                new ColumnRenderableSeriesViewModel {DataSeries = xyDataSeries},
+                new MountainRenderableSeriesViewModel {DataSeries = xyDataSeries},
+                new LineRenderableSeriesViewModel {DataSeries = xyDataSeries}
             };
+
+            ChartData = xyDataSeries;
+
+
         }
 
        
